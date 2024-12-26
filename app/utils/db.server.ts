18cd4108c4e822cb4be666a68c971049a02bcb2e
justDenzil <vikash.denzil@gmail.com>
2024-12-26 22:13:10 +0530

    added a user for mindwell-vercel for AWS IAM

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -10,7 +10,11 @@ import {
 } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-  region: process.env.COGNITO_REGION
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+  }
 });
 
 const docClient = DynamoDBDocumentClient.from(client);
